e066036b407bff5200a6636b99113282a7ee7750
Brendan Smithyman <brendan@bitsmithy.net>
2014-03-10 00:43:17 -0700

    Heat equation code.

--- heateqn.py
+++ heateqn.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+import numpy as np
 from common import *
 
+def perturb (model, lat, lng):
+    z = np.round(float(lat) + 90)
+    x = np.round(float(lng) + 180)
+    model[z,x] = model[z,x] + 1
+
 def disperse (inarr):
-    import numpy as np
 
     dims = inarr.shape
 
@@ -28,19 +33,25 @@ def disperse (inarr):
     bottom[-1,:] = np.mean(inarr[-1,:])
     bottom[:-1,:] = inarr[1:,:]
 
-    outarr = inarr + dt * ((left + right - 2*inarr)/dx**2 + (top + bottom - 2*inarr)/dz**2)
-
-    return outarr
+    inarr[:] = inarr + dt * ((left + right - 2*inarr)/dx**2 + (top + bottom - 2*inarr)/dz**2)
 
 if __name__ == '__main__':
 
     db = getmongodb()
     mcol = db.model
+    ipcol = db.ip
 
     res = mcol.find_one()
     model = bson2arr(res['model'])
 
-    model = disperse(model)
+    if ipcol.count() > 0:
+        for entry in ipcol.find():
+            id = entry['_id']
+            if (entry['lat']):
+                perturb(model, entry['lat'], entry['lng'])
+            ipcol.remove({'_id': id})
+
+    disperse(model)
     res['model'] = arr2bson(model)
 
     mcol.save(res)
